685f5bfddb7f2dc9762830f65e12eb207a80e97e
Rohit Kumar Rout <rohit.kumar.rout0075@gmail.com>
2023-03-13 14:30:03 +0530

    Weekly Assignment submitted

--- Week 01/Q1.c
+++ Week 01/Q1.c
@@ -4,7 +4,10 @@ void main(){
 int n=100;
 int i=1;
 while(i<=n){
-    if(i%3==0){
+    if(i%3==0 && i%5==0){
+        printf("Fizz, Buzz\n");
+    }
+    else if(i%3==0){
         printf("Fizz\n");
     }
     else if(i%5==0){
